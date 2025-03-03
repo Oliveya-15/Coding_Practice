408d13a9020c79de6c018be01f4bd735ebeccae5
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-03 21:48:07 +0530

    Day 1: P2 - Largest Number from an Array

---
+++ Day2_P2_3_3_2025.py
@@ -0,0 +1,7 @@
+#Find the Largest from an Array
+a=list(map(int, input("Enter the numbers: ").split()))
+large=a[0]
+for i in a:
+    if i > large:
+        large=i
+print("The Largest element is: ",large)
