51197024b3df44c1bc12c5d2a13be91af40871e3
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-03 21:34:30 +0530

    Day 1: Smallest Number from an Array

---
+++ Day1_3_3_2025.py
@@ -0,0 +1,7 @@
+#Find the Smallest Number in an Array
+a= list(map(int, input("enter the numbers: ").split()))
+small=a[0]
+for i in a:
+    if i < small:
+        small = i
+print("the smallest element is: ",small)
