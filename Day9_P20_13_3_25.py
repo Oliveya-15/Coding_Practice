ae65a96f537219e74d31129e28557ba8586b6842
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-13 21:53:34 +0530

    Day9 : P20 : Rotate array by K elements

--- Day9_P20_13_3_25.py
+++ Day9_P20_13_3_25.py
@@ -10,8 +10,10 @@ print(r)
 """
 a = 1,2,3,4,5,6,7
 k=2
-r = a[-k:] = [6,7]
-
+last = a[-k:] => [6,7]   
+bcz, -k start from backwards and as k value is 2 so it will start from -2 which have 6 then it will take 7 like this
+first = a[0:-k] => [1,2,3,4,5] take from 0th position till -k position which starts from 1 to 5
+add last + first => [6,7,1,2,3,4,5]
 
 """
 
