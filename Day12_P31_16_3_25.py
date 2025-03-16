a510cbc41703750fb4a55b6b8eaa713a3ba1f78b
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-16 17:21:51 +0530

    Day12 : P31 : Check whether a number is positive or negative

---
+++ Day12_P31_16_3_25.py
@@ -0,0 +1,19 @@
+#Check whether a number is positive or negative
+n=int(input("Enter the Number: "))
+if n>0:
+    print("The Number is Positive: ",n)
+elif n<0:
+    print("The Number is Negative: ",n)
+elif n==0:
+    print("The Number is Zero: ",n)
+else:
+    print("Not a Number")
+
+
+
+# Input: n=5
+# Output: Positive
+
+# Example2:
+# Input: n=-6
+# Output: Negative
