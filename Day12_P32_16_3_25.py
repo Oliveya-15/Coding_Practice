8ca6ecaab9e03092146c8e5eb5622e9bf3607ef9
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-16 17:25:02 +0530

    Day12 : P32 : Sum of first N Natural Numbers

---
+++ Day12_P32_16_3_25.py
@@ -0,0 +1,16 @@
+#Sum of first N Natural Numbers
+n=int(input("Enter the Range/Number: "))
+f=0
+for i in range(1,n+1):
+    f=f+i
+print("Sum of first N Natural Numbers is: ",f)
+
+
+# Input: N=5
+# Output: 15
+# Explanation: 1+2+3+4+5=15
+
+# Example 2:
+# Input: N=6
+# Output: 21
+# Explanation: 1+2+3+4+5+6=15
