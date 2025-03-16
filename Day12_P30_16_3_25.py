cdcac404154603cb13de99a6ace21061eefd31e3
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-16 17:18:00 +0530

    Day12 : P30 : Check whether a given number is even or odd

---
+++ Day12_P30_16_3_25.py
@@ -0,0 +1,15 @@
+#Check whether a given number is even or odd
+n=int(input("Enter the Number: "))
+if n%2 == 0:
+    print("The Number is Even: ",n)
+else:
+    print("The Number is Odd: ",n)
+
+# Input: n=5
+# Output: odd
+# Explanation: 5 is not divisible by 2.
+ 
+# Example 2:  
+# Input: n=6
+# Output: even
+# Explanation: 6 is divisible by 2.
