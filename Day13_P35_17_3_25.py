88048cb8a83a06f50119cd332645f429206fd121
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-17 21:52:44 +0530

    Day13 : P35 : Greatest of two numbers

---
+++ Day13_P35_17_3_25.py
@@ -0,0 +1,11 @@
+# Greatest of two numbers
+a=int(input("Enter the value of a: "))
+b=int(input("Enter the value of a: "))
+if a>b:
+    print("a is greater than b")
+else:
+    print("b is greater than a")
+
+# Input: 1 3
+# Output: 3
+# Explanation: Answer is 3,since 3 is greater than 1.
