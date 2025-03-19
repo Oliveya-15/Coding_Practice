47c3668adb40b80af8fdb3c5cf589747cd283af7
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-19 22:23:41 +0530

    Day15 : P45 : Check if a number is a Strong Number or not

---
+++ Day15_P45_19_3_25.py
@@ -0,0 +1,20 @@
+# Check if a number is a Strong Number or not
+n = int(input("Enter the number: "))
+m=n
+s=0
+while n>0:
+    r=n%10
+    f=1
+    for i in range(1,r+1):
+        f=f*i
+    s=s+f
+    n=n//10
+if s==m:
+    print("Strong")
+else:
+    print("Still Strong")
+
+
+# Input: N = 145
+# Output: Yes
+# Explanation: 1! + 4! + 5! = 145. Hence 145 is a strong number. 
