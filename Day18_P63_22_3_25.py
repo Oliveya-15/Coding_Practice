ad3bdc61ac9dd541f637a63fa6292e5ea9034b24
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-22 19:23:20 +0530

    Day18 : P63 : Convert Octal to Decimal

---
+++ Day18_P63_22_3_25.py
@@ -0,0 +1,13 @@
+# Convert Octal to Decimal
+n=input("enter the number as octal value: ")
+print(int(n,8))
+
+# Example 1:
+# Input: 345
+# Output: 229
+# Explanation: Decimal equivalent of given Octal expressionis 229
+
+# Example 2:
+# Input: 170
+# Output: 121
+# Explanation: Decimal equivalent of given Octal expression is121
