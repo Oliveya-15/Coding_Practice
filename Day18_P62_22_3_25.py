45ff19580ed61f7069a5e1e2b3a12aa5b688e816
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-22 19:23:52 +0530

    Day18 : P62 : Convert Decimal to Octal

---
+++ Day18_P62_22_3_25.py
@@ -0,0 +1,13 @@
+# Convert Decimal to Octal
+n=int(input("Enter the decimal value: "))
+print(oct(n)[2:])
+
+# Example 1:
+# Input:  17
+# Output: 21
+# Explanation: Octal Equivalent of 17 is 21
+
+# Example 2:
+# Input:  45
+# Output: 55
+# Explanation: Octal Equivalent of 45 is 55
