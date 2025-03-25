089f7b4357fe3edef2de805b8309e478dee7a5a9
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-25 19:15:17 +0530

    Day21 : P67 : Insertion Sort algorithm

---
+++ Day21_P67_25_3_25.py
@@ -0,0 +1,21 @@
+# Insertion Sort algorithm
+n=int(input("Enter the range: "))
+a=list(map(int,input("Enter the values: ").split()))
+for i in range(2,n):
+    temp=a[i]
+    j=i-1
+    while ((a[j]>temp) and (j>=1)):
+        a[j+1]=a[j]
+        j-=1
+    a[j+1]=temp
+print(a)
+
+# Example 1:
+# Input: N = 6, array[] = {13,46,24,52,20,9}
+# Output: 9,13,20,24,46,52
+# Explanation: After sorting the array is: 9, 13, 20, 24, 46, 52
+
+# Example 2:
+# Input: N=5, array[] = {5,4,3,2,1}
+# Output: 1,2,3,4,5
+# Explanation: After sorting the array is: 1, 2, 3, 4, 5
