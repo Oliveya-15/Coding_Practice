dd79dd48ee2ff92fc2538a5ae5dd287e4deb75cf
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-03-31 16:57:58 +0530

    Day26 : P87 : Change every letter with next lexicographic alphabet

---
+++ Day26_P87_31_3_25.py
@@ -0,0 +1,21 @@
+# Change every letter with next lexicographic alphabet
+a=input("Enter the string: ")
+r=""
+for i in a:
+    if i=='z':
+        r=r+'a'
+    elif i=='Z':
+        r=r+'A'
+    else:
+        r=r+chr(ord(i)+1)
+print(r)
+
+# Example 1:
+# Input: string str = “abcdxyz”
+# Output: bcdeyza
+# Explanation:
+# a -> b
+# b -> c
+# c -> d
+# x -> y
+# y -> z   like this
