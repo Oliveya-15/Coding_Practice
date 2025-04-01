ba8759c0b5b6a98e13e7203c82b1f4dbc37e57d9
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-04-01 19:29:10 +0530

    Day27 : P88 : Find the largest word in a String

---
+++ Day27_P88_1_4_25.py
@@ -0,0 +1,16 @@
+# Find the largest word in a String
+a = input("Enter the string: ")
+b=a.split()
+print(max(b))
+
+
+# Example 1:
+# Input: string s=”Google Doc”
+# Output: “Google”
+
+# Explanation: Google is the largest word in the given string.
+
+# Example 2:
+# Input: string s=”Microsoft Teams”
+# Output: “Microsoft”
+# Explanation: Microsoft is the largest word in the given string
