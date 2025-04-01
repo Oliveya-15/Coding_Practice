d02b0b0263e5c99e8c9fed790049b694ecdc80ab
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-04-01 20:11:25 +0530

    Day27 : P90 : Count the number of words in a given string

---
+++ Day27_P90_1_4_25.py
@@ -0,0 +1,10 @@
+# Count the number of words in a given string
+a=input("Enter the String: ")
+c=1
+for i in a:
+    if i==" ":
+        c=c+1
+print(c)
+
+# Input : my name is oli
+# Output : 4 [as 4 words are present in the sentence/string]
