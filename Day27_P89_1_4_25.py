195a7c2c5c8255043b5c1452eaf59a03a2eb7cb7
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-04-01 20:04:02 +0530

    Day27 : P89 : Write a program to sort characters in a string

---
+++ Day27_P89_1_4_25.py
@@ -0,0 +1,14 @@
+# Write a program to sort characters in a string
+a = input("enter the string: ")
+s = sorted(a)
+print("".join(s))
+
+# Example 1:
+# Input: String str = “zxcbg”
+# Output: bcgxz
+# Explanation: After sorting we get string as bcgxz
+
+# Example 2:
+# Input: String str = “edcba”
+# Output: abcde
+# Explanation: After sorting we get string as abcde
