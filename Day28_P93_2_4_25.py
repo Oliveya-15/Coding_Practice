c9015da41bf20e708f5290e2f5f2b6fd2e2f10de
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-04-02 20:37:53 +0530

    Day28 : P93 : Concatenate one string to another

---
+++ Day28_P93_2_4_25.py
@@ -0,0 +1,8 @@
+# Concatenate one string to another
+a = input("Enter first string: ")
+b = input("Enter second string: ")
+print(a+b)
+
+# Input : a = "Hello"
+#         b = "World"
+# Output : HelloWorld
