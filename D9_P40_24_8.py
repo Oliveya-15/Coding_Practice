0bea346c34dcfaf88027671194041a05b4bd7a6b
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2025-08-24 14:32:49 +0530

    Practice_Day9_24/8/25

---
+++ D9_P40_24_8.py
@@ -0,0 +1,13 @@
+#Day9 P40
+#Factors of a Given Number print only prime numbers
+n=int(input("Enter the number: "))
+for i in range(2,n+1):
+    c=0
+    if n%i==0:
+        for j in range(1,i+1):
+            if i%j==0:
+                c=c+1
+        if c==2:
+            print(i)
+
+
