6d6883a4bc21beab4403344d95b600ba8f9b2cd2
Oliveya Choudhury <choudhuryoliveya15@gmail.com>
2026-08-23 22:52:06 +0530

    College Codes Day 1

---
+++ Col6.py
@@ -0,0 +1,13 @@
+# Numpy 2 * 3 Matrix of ones
+# 3 * 3 identity matrix
+# 3 * 3 random integers matrix 1 to 20
+
+# - EXECUTION IN GOOGLE COLAB -
+
+import numpy as np
+a = np.ones((2,3))
+b = np.eye(3)
+c = np.random.randint(1, 21, (3, 3))
+print(c)
+print(b)
+print(a)
